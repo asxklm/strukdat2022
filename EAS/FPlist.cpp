d04a1490b98616ffb598bcffedcb16bf3028464c
asxklm <113827418+asxklm@users.noreply.github.com>
2022-12-19 22:00:44 +0700

    Add files via upload

---
+++ EAS/FPlist.cpp
@@ -0,0 +1,241 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 0x3f3f3f3f
+
+bool foundDFSNode = false;
+
+class Graph
+{
+  int numVertices;
+  list<pair<int, int>> *adjLists;
+  map<int, bool> visitedDfsNode;
+  bool *visited;
+
+  public:
+    Graph(int vertices);
+    void addEdge(int src, int dest, int weight);
+    void BFS(int startVertex, int endVertex);
+    void shortestPath(int src, int dest);
+    void DFS(int v, int end);
+    void showList();
+};
+
+// Create a graph with given vertices,
+// and maintain an adjacency list
+Graph::Graph(int vertices)
+{
+  numVertices = vertices;
+  adjLists = new list<pair<int, int>>[vertices];
+}
+
+// Add edges to the graph
+void Graph::addEdge(int src, int dest, int weight)
+{
+  adjLists[src].push_back(make_pair(dest, weight));
+  // adjLists[dest].push_back(src);
+}
+
+// BFS algorithm
+void Graph::BFS(int startVertex, int endVertex)
+{
+  visited = new bool[numVertices];
+  for (int i = 0; i < numVertices; i++)
+    visited[i] = false;
+
+  list<int> queue;
+
+  visited[startVertex] = true;
+  queue.push_back(startVertex);
+
+  // list<int>::iterator i;
+  list<pair<int, int>>::iterator i;
+
+  while (!queue.empty())
+  {
+    int currVertex = queue.front();
+    // Fungsi memberhentikan BFS
+    if (currVertex == endVertex)
+    {
+      break;
+    }
+    //
+    cout << "(V" << currVertex << ")";
+    queue.pop_front();
+
+    for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i)
+    {
+      int adjVertex = (*i).first;
+      if (!visited[adjVertex])
+      {
+        visited[adjVertex] = true;
+        queue.push_back(adjVertex);
+      }
+    }
+  }
+  cout << endl;
+}
+
+void Graph::shortestPath(int src, int dest)
+{
+    // Create a set to store vertices that are being
+    // processed
+    set< pair<int, int> > setds;
+ 
+    // Create a vector for distances and initialize all
+    // distances as infinite (INF)
+    vector<int> dist(numVertices, INF);
+ 
+    // Insert source itself in Set and initialize its
+    // distance as 0.
+    setds.insert(make_pair(0, src));
+    dist[src] = 0;
+ 
+    /* Looping till all shortest distance are finalized
+       then setds will become empty    */
+    while (!setds.empty())
+    {
+
+        // The first vertex in Set is the minimum distance
+        // vertex, extract it from set.
+        pair<int, int> tmp = *(setds.begin());
+        setds.erase(setds.begin());
+ 
+        // vertex label is stored in second of pair (it
+        // has to be done this way to keep the vertices
+        // sorted distance (distance must be first item
+        // in pair)
+        int u = tmp.second;
+ 
+        // 'i' is used to get all adjacent vertices of a vertex
+        list<pair<int, int>>::iterator i;
+        for (i = adjLists[u].begin(); i != adjLists[u].end(); ++i)
+        {   
+            // Get vertex label and weight of current adjacent
+            // of u.
+            int v = (*i).first;
+            int weight = (*i).second;
+            //    If there is shorter path to v through u.
+            if (dist[v] > dist[u] + weight)
+            {
+                if (dist[v] != INF)
+                {
+                  setds.erase(setds.find(make_pair(dist[v], v)));
+                }
+ 
+                // Updating distance of v
+                dist[v] = dist[u] + weight;
+                setds.insert(make_pair(dist[v], v));
+            }
+        }
+    }
+ 
+    // Print shortest distances stored in dist[]
+    printf("Vertex Distance from Source\n");
+    for (int i = 0; i < numVertices; ++i)
+    {
+      if (i == dest)
+      {
+        break;
+      }
+      
+      printf("%d \t\t %d\n", i, dist[i]);
+    }
+}
+
+void Graph::DFS(int v, int end)
+{
+    if (foundDFSNode == false)
+    {
+    // Mark the current node as visited and
+    // print it
+    visitedDfsNode[v] = true;
+    cout << v << " ";
+
+    // Recur for all the vertices adjacent
+    // to this vertex
+    list<pair<int, int>>::iterator i;
+ 
+    // for (i = adjMatrix[v].begin(); i != adjMatrix[v].end(); ++i)
+    // {
+    //   if (visitedDfsNode[(*i).first] == end)
+    //   {
+    //     foundDFSNode = true;
+    //     break;
+    //   }
+    // }
+
+      for (i = adjLists[v].begin(); i != adjLists[v].end(); ++i)
+      {
+
+        if (v == end)
+        {
+          foundDFSNode = true;
+          // break;
+        }
+        if (!visitedDfsNode[(*i).first])
+        {
+          DFS((*i).first, end);
+        }
+      }
+    }
+    
+    else
+    {
+
+    }
+}
+
+void Graph::showList()
+{
+  for (int i = 0; i < numVertices; i++)
+  {
+    cout << i << ": ";
+    list<pair<int, int>>::iterator it;
+      for(it = adjLists[i].begin(); it != adjLists[i].end(); ++it){
+        cout << (*it).first << " ";
+      }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  Graph g(21);
+  g.addEdge(1, 3, 1);
+  g.addEdge(1, 2, 2);
+  g.addEdge(3, 4, 2);
+  g.addEdge(4, 5, 1);
+  g.addEdge(4, 17, 2);
+  g.addEdge(5, 6, 3);
+  g.addEdge(6, 7, 3);
+  g.addEdge(6, 18, 2);
+  g.addEdge(6, 17, 4);
+  g.addEdge(7, 8, 1);
+  g.addEdge(8, 18, 4);
+  g.addEdge(18, 19, 4);
+  g.addEdge(19, 20, 4);
+  g.addEdge(20, 9, 2);
+  g.addEdge(9, 10, 3);
+  g.addEdge(9, 8, 3);
+  g.addEdge(10, 11, 2);
+  g.addEdge(2, 12, 5);
+  g.addEdge(13, 12, 3);
+  g.addEdge(13, 1, 2);
+  g.addEdge(12, 11, 2);
+  g.addEdge(15, 13, 3);
+  g.addEdge(15, 14, 4);
+  g.addEdge(14, 11, 3);
+  g.addEdge(16, 15, 4);
+  g.addEdge(16, 17, 2);
+
+  cout << endl << "==================================== BFS" << endl;
+  g.BFS(1, 5);
+  cout << endl << "==================================== DFS" << endl;
+  g.DFS(1, 5);
+  cout << endl << endl << "==================================== Dijkstra" << endl;
+  g.shortestPath(1, 5);
+  cout << endl << "==================================== Adj. Lists" << endl;
+  g.showList();
+
+  return 0;
+}
---
+++ EAS/FPmatrix.cpp
@@ -0,0 +1,266 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 0x3f3f3f3f
+
+bool foundDFSNode = false;
+
+class Graph
+{
+  int numVertices;
+  list<pair<int, int>> *adjMatrix;
+  map<int, bool> visitedDfsNode;
+  bool *visited;
+
+  public:
+    Graph(int vertices);
+    void addEdge(int src, int dest, int weight);
+    void BFS(int startVertex, int endVertex);
+    void shortestPath(int src, int dest);
+    void DFS(int v, int end);
+    void showMatrix();
+};
+
+Graph::Graph(int vertices)
+{
+  numVertices = vertices;
+  adjMatrix = new list<pair<int, int>>[vertices];
+}
+
+// Add edges to the graph
+void Graph::addEdge(int src, int dest, int weight)
+{
+  adjMatrix[src].push_back(make_pair(dest, weight));
+  adjMatrix[src].sort();
+}
+
+void Graph::BFS(int startVertex, int endVertex)
+{
+  visited = new bool[numVertices];
+  for (int i = 0; i < numVertices; i++)
+    visited[i] = false;
+
+  list<int> queue;
+
+  visited[startVertex] = true;
+  queue.push_back(startVertex);
+
+  list<pair<int, int>>::iterator i;
+
+  while (!queue.empty())
+  {
+    int currVertex = queue.front();
+    // Fungsi memberhentikan BFS
+    if (currVertex == endVertex)
+    {
+      break;
+    }
+    //
+    cout <<  currVertex << " ";
+    queue.pop_front();
+
+    for (i = adjMatrix[currVertex].begin(); i != adjMatrix[currVertex].end(); ++i)
+    {
+      int adjVertex = (*i).first;
+      if (!visited[adjVertex])
+      {
+        visited[adjVertex] = true;
+        queue.push_back(adjVertex);
+      }
+    }
+  }
+  cout << endl;
+}
+
+void Graph::shortestPath(int src, int dest)
+{
+    // Create a set to store vertices that are being
+    // processed
+    set<pair<int, int>> setds;
+ 
+    // Create a vector for distances and initialize all
+    // distances as infinite (INF)
+    vector<int> dist(numVertices, INF);
+ 
+    // Insert source itself in Set and initialize its
+    // distance as 0.
+    setds.insert(make_pair(0, src));
+    dist[src] = 0;
+ 
+    /* Looping till all shortest distance are finalized
+       then setds will become empty    */
+    while (!setds.empty())
+    {
+
+        // The first vertex in Set is the minimum distance
+        // vertex, extract it from set.
+        pair<int, int> tmp = *(setds.begin());
+        setds.erase(setds.begin());
+ 
+        // vertex label is stored in second of pair (it
+        // has to be done this way to keep the vertices
+        // sorted distance (distance must be first item
+        // in pair)
+        int u = tmp.second;
+ 
+        // 'i' is used to get all adjacent vertices of a vertex
+        list<pair<int, int>>::iterator i;
+        for (i = adjMatrix[u].begin(); i != adjMatrix[u].end(); ++i)
+        {   
+            // Get vertex label and weight of current adjacent
+            // of u.
+            int v = (*i).first;
+            int weight = (*i).second;
+            //    If there is shorter path to v through u.
+            if (dist[v] > dist[u] + weight)
+            {
+                if (dist[v] != INF)
+                {
+                  setds.erase(setds.find(make_pair(dist[v], v)));
+                }
+ 
+                // Updating distance of v
+                dist[v] = dist[u] + weight;
+                setds.insert(make_pair(dist[v], v));
+            }
+        }
+    }
+ 
+    // Print shortest distances stored in dist[]
+    printf("Vertex Distance from Source\n");
+    for (int i = 0; i < numVertices; ++i)
+    {
+      if (i == dest)
+      {
+        break;
+      }
+      
+      printf("%d \t\t %d\n", i, dist[i]);
+    }
+}
+
+void Graph::DFS(int v, int end)
+{
+    if (foundDFSNode == false)
+    {
+    // Mark the current node as visited and
+    // print it
+    visitedDfsNode[v] = true;
+    cout << v << " ";
+
+    // Recur for all the vertices adjacent
+    // to this vertex
+    list<pair<int, int>>::iterator i;
+ 
+    // for (i = adjMatrix[v].begin(); i != adjMatrix[v].end(); ++i)
+    // {
+    //   if (visitedDfsNode[(*i).first] == end)
+    //   {
+    //     foundDFSNode = true;
+    //     break;
+    //   }
+    // }
+
+      for (i = adjMatrix[v].begin(); i != adjMatrix[v].end(); ++i)
+      {
+
+        if (v == end)
+        {
+          foundDFSNode = true;
+          // break;
+        }
+        if (!visitedDfsNode[(*i).first])
+        {
+          DFS((*i).first, end);
+        }
+      }
+    }
+    
+    else
+    {
+
+    }
+}
+
+void Graph::showMatrix()
+{
+  for (int i = 0; i < numVertices; i++)
+  {
+    list<pair<int, int>>::iterator it;
+
+    int track = 0;
+    int through = 0;
+    for (it = adjMatrix[i].begin(); it != adjMatrix[i].end(); ++it)
+    {
+      // int track = 0;
+      int temp = (*it).first;
+      if (through == 0)
+      {
+        while (track < temp)
+        {
+          cout << "0 ";
+          ++track;
+          through++;
+        }
+      }
+      else
+      {
+        while ((track + 1) < temp)
+        {
+          cout << "0 ";
+          ++track;
+          through++;
+        }
+      }
+      cout << "1 ";
+      through++;
+    }
+    while ((numVertices - through) > 0)
+    {
+      through++;
+      cout << "0 ";
+    }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  Graph g(21);
+  g.addEdge(1, 3, 1);
+  g.addEdge(1, 2, 2);
+  g.addEdge(3, 4, 2);
+  g.addEdge(4, 5, 1);
+  g.addEdge(4, 17, 2);
+  g.addEdge(5, 6, 3);
+  g.addEdge(6, 7, 3);
+  g.addEdge(6, 18, 2);
+  g.addEdge(6, 17, 4);
+  g.addEdge(7, 8, 1);
+  g.addEdge(8, 18, 4);
+  g.addEdge(18, 19, 4);
+  g.addEdge(19, 20, 4);
+  g.addEdge(20, 9, 2);
+  g.addEdge(9, 10, 3);
+  g.addEdge(9, 8, 3);
+  g.addEdge(10, 11, 2);
+  g.addEdge(2, 12, 5);
+  g.addEdge(13, 12, 3);
+  g.addEdge(13, 1, 2);
+  g.addEdge(12, 11, 2);
+  g.addEdge(15, 13, 3);
+  g.addEdge(15, 14, 4);
+  g.addEdge(14, 11, 3);
+  g.addEdge(16, 15, 4);
+  g.addEdge(16, 17, 2);
+
+  cout << endl << "==================================== BFS" << endl;
+  g.BFS(1, 5);
+  cout << endl << "==================================== DFS" << endl;
+  g.DFS(1, 5);
+  cout << endl << endl << "==================================== Dijkstra" << endl;
+  g.shortestPath(1, 5);
+  cout << endl << "==================================== Adj. Lists" << endl;
+  g.showMatrix();
+
+  return 0;
+}
